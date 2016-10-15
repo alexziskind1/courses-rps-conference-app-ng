200fa342616e476a25f7bf834ef0d609559954e7
Alexander Ziskind <alex@nuvious.com>
2016-10-15 12:59:44 -0400

    Fixes after merge

--- rps-conf-ng/app/app.routes.ts
+++ rps-conf-ng/app/app.routes.ts
@@ -4,9 +4,8 @@ import { HomeComponent } from './home.component';
 import { StartComponent } from './start.component';
 import { SomeOtherComponent } from './some-other.component';
 import { DetailsComponent } from './details.component';
+import { MapComponent } from './map.component';
 
-// Purely an example
-// Change the routes/components to meet your needs
 export const routes: Routes = [
   {
     path: '',
@@ -17,9 +16,12 @@ export const routes: Routes = [
     path: "home/:id",
     component: HomeComponent
   },
-  // '/someNavPage' pushed on nav stack using `page-router-outlet` (ie, push on a detail view with no drawer
   {
     path: "details/:id",
     component: DetailsComponent
+  },
+  {
+    path: "map/:id",
+    component: MapComponent
   }
 ];
