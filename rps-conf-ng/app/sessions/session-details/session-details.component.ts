a056c652a7abbddee3571042ab3446672a2ee6bc
Alexander Ziskind <alex@nuvious.com>
2016-10-27 16:09:05 -0400

    session details description animated slide down in native

--- rps-conf-ng/app/sessions/session-details/session-details.component.ts
+++ rps-conf-ng/app/sessions/session-details/session-details.component.ts
@@ -11,11 +11,14 @@ import { GestureEventData, SwipeGestureEventData, SwipeDirection } from 'ui/gest
 import { Page } from "ui/page";
 import { Button } from 'ui/button';
 import { Label } from 'ui/label';
+import { GridLayout } from 'ui/layouts/grid-layout';
 
 //app
 import { SessionsService } from '../../services/sessions.service';
 import { SessionModel } from '../shared/session.model';
 
+
+declare var UIView, CGAffineTransformMakeTranslation;
 
 @Component({
   moduleId: module.id,
@@ -25,10 +28,32 @@ import { SessionModel } from '../shared/session.model';
 })
 export class SessionDetailsComponent implements OnInit {
 
+  private _lblDescShortHeight: number;
+  private _lblDescHeight: number;
+  private _animationDurationSeconds: number = 0.3;
+
   public session: SessionModel;
 
-  @ViewChild('btnDesc') btnDesc: ElementRef;
-  @ViewChild('lblDesc') lblDesc: ElementRef;
+  @ViewChild('btnDesc') btnDescRef: ElementRef;
+  @ViewChild('lblDesc') lblDescRef: ElementRef;
+  @ViewChild('lblDescShort') lblDescShortRef: ElementRef;
+  @ViewChild('speakersContainer') speakersContainerRef: ElementRef;
+
+  public get btnDesc(): Button {
+    return this.btnDescRef.nativeElement;
+  }
+
+  public get lblDesc(): Label {
+    return this.lblDescRef.nativeElement;
+  }
+
+  public get lblDescShort(): Label {
+    return this.lblDescShortRef.nativeElement;
+  }
+
+  public get speakersContainer(): GridLayout {
+    return this.speakersContainerRef.nativeElement;
+  }
 
   constructor(private _page: Page, private _sessionsService: SessionsService, private route: ActivatedRoute,
     private location: Location, private routerExtensions: RouterExtensions) {
@@ -45,6 +70,23 @@ export class SessionDetailsComponent implements OnInit {
           this.session = session;
         });
     });
+  }
+
+  public sessionWrapperLoaded() {
+    //let speakersContainer = <GridLayout>this.speakersContainer.nativeElement;
+    //let btnDesc = <Button>this.btnDesc.nativeElement;
+    //let lblDesc = <Label>this.lblDesc.nativeElement;
+    //let lblDescShort = <Label>this.lblDescShort.nativeElement;
+
+    this._lblDescHeight = this.lblDesc.getMeasuredHeight()
+    this._lblDescShortHeight = this.lblDescShort.getMeasuredHeight();
+
+    this.speakersContainer.marginTop = this._lblDescShortHeight;
+    this.lblDesc.opacity = 0;
+
+    if (this.session.description.length === this.session.descriptionShort.length) {
+      this.btnDesc.visibility = 'collapsed';
+    }
   }
 
   public backSwipe(args: SwipeGestureEventData) {
@@ -70,16 +112,29 @@ export class SessionDetailsComponent implements OnInit {
   }
 
   public toogleDescription() {
-    let btn = <Button>this.btnDesc.nativeElement;
-    let lbl = <Label>this.lblDesc.nativeElement;
-    if (btn.text === 'MORE') {
-      btn.text = 'LESS';
-      lbl.text = this.session.description;
+
+    let transY = this._lblDescHeight - this._lblDescShortHeight;
+
+    if (this.btnDesc.text === 'MORE') {
+
+      UIView.animateWithDurationAnimations(this._animationDurationSeconds, () => {
+        this.speakersContainer.ios.transform = CGAffineTransformMakeTranslation(0, transY);
+        this.lblDesc.ios.alpha = 1;
+        this.lblDescShort.ios.alpha = 0;
+      });
+
+      this.speakersContainer.marginTop = this._lblDescHeight;
+      this.btnDesc.text = 'LESS';
     }
     else {
-      btn.text = 'MORE';
-      lbl.text = this.session.descriptionShort;
-      //scroll.scrollToVerticalOffset(0, false);
+      UIView.animateWithDurationAnimations(this._animationDurationSeconds, () => {
+        this.speakersContainer.ios.transform = CGAffineTransformMakeTranslation(0, 0);
+        this.lblDesc.ios.alpha = 0;
+        this.lblDescShort.ios.alpha = 1;
+      });
+
+      this.speakersContainer.marginTop = this._lblDescShortHeight;
+      this.btnDesc.text = 'MORE';
     }
   }
 
