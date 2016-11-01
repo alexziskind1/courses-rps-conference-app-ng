0ddf02cb2d93269f49971ea966a5270145f4a484
Alexander Ziskind <alex@nuvious.com>
2016-11-01 09:54:52 -0400

    added pure JS animation of details description

--- rps-conf-ng/app/sessions/session-details/session-details.component.ts
+++ rps-conf-ng/app/sessions/session-details/session-details.component.ts
@@ -11,6 +11,9 @@ import { GestureEventData, SwipeGestureEventData, SwipeDirection } from 'ui/gest
 import { Page } from "ui/page";
 import { Button } from 'ui/button';
 import { Label } from 'ui/label';
+import { TextView } from 'ui/text-view';
+import { Layout } from 'ui/layouts/layout';
+import { View } from 'ui/core/view';
 
 //app
 import { SessionsService } from '../../services/sessions.service';
@@ -29,6 +32,7 @@ export class SessionDetailsComponent implements OnInit {
 
   @ViewChild('btnDesc') btnDesc: ElementRef;
   @ViewChild('lblDesc') lblDesc: ElementRef;
+  @ViewChild('lblDesc1') lblDesc1: ElementRef;
 
   constructor(private _page: Page, private _sessionsService: SessionsService, private route: ActivatedRoute,
     private location: Location, private routerExtensions: RouterExtensions) {
@@ -45,6 +49,19 @@ export class SessionDetailsComponent implements OnInit {
           this.session = session;
         });
     });
+  }
+
+  private descHeight = 20;
+
+  public descWrapperLoaded(wrapper: Layout, txtDesc: TextView, lblDesc: Label) {
+    let h1 = lblDesc.getMeasuredHeight();
+
+    this.descHeight = h1;
+    lblDesc.visibility = 'collapsed';
+    wrapper.height = 60;
+
+    txtDesc.ios.scrollEnabled = false;
+    console.log('SCROLL enabled: ' + txtDesc.ios.scrollEnabled);
   }
 
   public backSwipe(args: SwipeGestureEventData) {
@@ -69,18 +86,62 @@ export class SessionDetailsComponent implements OnInit {
       });
   }
 
-  public toogleDescription() {
+  public toogleDescription(wrapper: Layout) {
     let btn = <Button>this.btnDesc.nativeElement;
     let lbl = <Label>this.lblDesc.nativeElement;
+
+
+
+
     if (btn.text === 'MORE') {
       btn.text = 'LESS';
-      lbl.text = this.session.description;
+      changeHeight(wrapper, quad, 1000, this.descHeight, 60);
+      //lbl.text = this.session.description;
     }
     else {
       btn.text = 'MORE';
-      lbl.text = this.session.descriptionShort;
+      //lbl.text = this.session.descriptionShort;
       //scroll.scrollToVerticalOffset(0, false);
+      changeHeight(wrapper, quad, 1000, 60, this.descHeight);
     }
   }
 
+}
+
+
+function changeHeight(view: View, delta: (p) => {}, duration?: number, to?: number, from?: number) {
+  var from = from || 1;
+  var to = to || 500;
+
+  animate({
+    delay: 5,
+    duration: duration || 1000, // 1 sec by default
+    delta: delta,
+    step: function (delta) {
+      view.height = (to - from) * delta + from;
+    }
+  });
+}
+
+
+function animate(opts) {
+  var start = new Date();
+
+  var id = setInterval(function () {
+    var timePassed = <any>(new Date()) - <any>start;
+    var progress = timePassed / opts.duration;
+    if (progress > 1) progress = 1;
+
+    var delta = opts.delta(progress);
+    opts.step(delta);
+
+    if (progress == 1) {
+      clearInterval(id);
+    }
+  }, opts.delay || 10);
+
+}
+
+function quad(progress) {
+  return Math.pow(progress, 2);
 }
